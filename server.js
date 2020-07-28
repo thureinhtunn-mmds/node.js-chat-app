358a1cdd48cbeb70af180009480896f717968a1d
thure1n <thurein.htunn@mm-digital-solutions.com>
2020-07-28 23:43:25 +0630

    text messages from api success

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const { env } = require('process');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const port = process.env.port || 5000;
 //require for file serve
 var fs = require('fs');
 const { isObject } = require('util');
+var bodyParser  =  require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : true,
+}));
 
 const formatedMessage = require('./utils/message');
 // const Chat = require('./models/Chat');
 // const connect = require('./dbconnection');
 
-const port = process.env.port || 5000;
 var users = [];
 app.use(express.static(__dirname + '/public'));
 
@@ -27,6 +32,16 @@ io.on('connection',(socket)=>{
         socket.broadcast.emit('user_connected',username);
     });
 
+
+    app.post('/message',function(req,res){
+        var socketId = users[req.body.receiver];
+        var senderId = users[req.body.sender];
+        console.log(senderId);
+        io.to(socketId).emit('incoming_message', formatedMessage(req.body.sender,req.body.receiver,req.body.message));
+        socket.broadcast.to(senderId).emit('outgoing_message',formatedMessage(req.body.sender,req.body.receiver,req.body.message));
+        res.status(200).send(req.body);
+    });
+
     socket.on('message',(data)=>{
         var socketId = users[data.receiver];
         io.to(socketId).emit('new_message', formatedMessage(data.sender,data.receiver,data.message));
@@ -40,7 +55,7 @@ io.on('connection',(socket)=>{
     // });
 
     socket.on('image',(msg)=>{
-        console.log(msg);
+        //console.log(msg);
         var socketId = users[msg.receiver];
         socket.to(socketId).emit('userimage', msg);
     });
